395f20154679548c516d86b390a298c6e887514a
sherlock <sujanmondal916@gmail.com>
2021-01-20 22:31:14 +0530

    floating button added no function added of floating button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import SearchForm from "./components/SearchForm";
-import { Grid, CssBaseline } from "@material-ui/core";
+import { Grid, CssBaseline, Fab, makeStyles } from "@material-ui/core";
 import ResultData from "./components/ResultData";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { mdiPlus } from "@mdi/js";
+import { Icon } from "@mdi/react";
+
+const useStyles = makeStyles((theme) => ({
+  fab: {
+    position: "absolute",
+    bottom: theme.spacing(3),
+    right: theme.spacing(2),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
     <>
       <Provider store={store}>
@@ -13,6 +24,14 @@ function App() {
           <SearchForm />
           <ResultData />
         </Grid>
+        <Fab
+          size="large"
+          aria-label="add new"
+          color="primary"
+          className={classes.fab}
+        >
+          <Icon path={mdiPlus} size={1.5} title="Add" />
+        </Fab>
       </Provider>
     </>
   );
